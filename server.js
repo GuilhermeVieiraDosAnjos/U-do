f7e034c298b863d822a4eea288819a859f773153
Guilherme Vieira <guilhermevieiradosanjos02@gmail.com>
2023-11-28 16:09:06 -0300

    retirando o helmet

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-require('dotenv').config()//Referente as variaveis de ambiente, que estão configuradas no .env
+
+// Importing necessary modules
+require('dotenv').config() // Referente as variaveis de ambiente, que estão configuradas no .env
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
-mongoose.connect(process.env.CONNECTIONSTRING)
-    .then(()=>{
-        console.log('Conectei a base de dados')
-        app.emit('Conectado')
-    }).catch(e=>console.log(e));
 const session = require('express-session');
 const MongoStore = require('connect-mongo');
 const flash = require('connect-flash');
@@ -14,25 +11,24 @@ const routes = require('./router');
 const path = require('path');
 const helmet = require('helmet');
 const csrf = require('csurf');
-const {middlewareGlobal, checkCSRFError, csrfMiddleware } = require('./src/middlewares/middleware');
+const { middlewareGlobal, checkCSRFError, csrfMiddleware } = require('./src/middlewares/middleware');
 const { log } = require('console');
 const EventEmitter = require('events')
 
-//Limite de Listeners
+// Setting up the maximum number of listeners
 EventEmitter.setMaxListeners(15)
 
+// Connecting to the database
+mongoose.connect(process.env.CONNECTIONSTRING)
+    .then(() => {
+        console.log('Conectei a base de dados')
+        app.emit('Conectado')
+    }).catch(e => console.log(e));
 
-
-app.use(helmet());//Verificar se o helmet não ta dando b.o
-app.use(express.urlencoded({extended : true}));
-app.use(express.static(path.resolve(__dirname, 'public')))
-
-//css
-app.use(express.static(path.join(__dirname, 'public') ));
-
+// Configuring the session middleware
 const sessionOptions = session({
     secret: 'thismysecret',
-    store:  MongoStore.create({
+    store: MongoStore.create({
         mongoUrl: process.env.CONNECTIONSTRING,
         mongooseConnection: mongoose.connection,
         collectionName: 'sessions'
@@ -44,26 +40,34 @@ const sessionOptions = session({
     },
     httpOnly: true
 });
-/*Ok, pelo o que eu descobri temos que retirar o session quando iniciamos o MongoStore, os parametros tem que ser passados dessa forma, com o create e sem o new, para que dê tudo certo*/
 
+// Configuring the app
+// app.use(helmet()); // Verificar se o helmet não ta dando b.o
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve(__dirname, 'public')));
+
+// Serving CSS files
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(sessionOptions);
 app.use(flash());
 app.set('views', path.resolve(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
-//CSRF
-app.use(csrf())
+// Configuring the CSRF middleware
+app.use(csrf());
 
-//Nossos próprios middlewares
+// Using custom middlewares
 app.use(middlewareGlobal);
 app.use(checkCSRFError);
 app.use(csrfMiddleware);
 app.use(routes);
 
-app.on('Conectado', ()=>{
-    app.listen(3006, ()=>{
+app.on('Conectado', () => {
+    app.listen(3006, () => {
         console.log('Servidor executando na porta 3006')
-        console.log('Acessar  http://localhost:3006')
+        console.log('Acessar http://localhost:3006')
     })
 })
+//
+//This code has been updated with proper commenting to provide a clear and concise understanding of the code..</s>
